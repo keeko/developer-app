2f946868cada55584757dcbd861c25c663d9f2eb
gossi <github@gos.si>
2016-01-27 02:25:11 +0100

    Update to keeko 0.2

--- src/DeveloperApplication.php
+++ src/DeveloperApplication.php
@@ -1,37 +1,37 @@
 <?php
-
 namespace keeko\application\developer;
 
-use keeko\core\application\AbstractApplication;
-use Symfony\Component\HttpFoundation\Request;
+use keeko\core\model\ModuleQuery;
+use keeko\core\package\AbstractApplication;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use keeko\core\model\Api;
-use keeko\core\model\ApiQuery;
-use keeko\core\model\Module;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\HttpFoundation\Response;
-use keeko\core\model\ModuleQuery;
 
+/**
+ * Developer Application
+ * 
+ * @license MIT
+ * @author gossi
+ */
 class DeveloperApplication extends AbstractApplication {
 	
 	private $modules;
-	
-	/* (non-PHPdoc)
-	 * @see \keeko\core\application\AbstractApplication::run()
-	*/
+
+	/**
+	 * @param Request $request
+	 */
 	public function run(Request $request) {
-	
 		$routes = $this->generateRoutes();
 		$response = new Response();
 		$context = new RequestContext($this->getAppPath());
 		$matcher = new UrlMatcher($routes, $context);
-		
-		$twig = $this->getTwig();
+
 		$prefs = $this->service->getPreferenceLoader()->getSystemPreferences();
-	
+		
 		try {
 			$path = str_replace('//', '/', '/' . $this->getDestinationPath());
 			$match = $matcher->match($path);
@@ -39,74 +39,76 @@ class DeveloperApplication extends AbstractApplication {
 			$route = $match['_route'];
 			$css = [];
 			$scripts = [];
-			
+				
 			switch ($route) {
 				case 'json':
 					$module = $this->findModuleBySlug($match['module']);
-					
+						
 					if ($module !== null) {
 						$extra = $module['package']->getExtra();
 						$api = $extra['keeko']['module']['api'];
 						$api['basePath'] = trim($prefs->getApiUrl() . $module['slug'], '/');
-						
+		
 						return new JsonResponse($api);
 					}
 					break;
-				
+		
 				case 'reference':
 				case 'module':
 					$current = '';
 					$content = $main;
 					if ($route == 'module') {
 						$scripts = [
-							'components/swagger-ui/dist/lib/shred.bundle.js',
-							'components/swagger-ui/dist/lib/jquery.slideto.min.js',
-							'components/swagger-ui/dist/lib/jquery.wiggle.min.js',
-							'components/swagger-ui/dist/lib/jquery.ba-bbq.min.js',
-							'components/swagger-ui/dist/lib/handlebars-1.0.0.js',
-							'components/swagger-ui/dist/lib/underscore-min.js',
-							'components/swagger-ui/dist/lib/backbone-min.js',
-							'components/swagger-ui/dist/lib/swagger.js',
-							'components/swagger-ui/dist/swagger-ui.min.js',
-							'components/swagger-ui/dist/lib/highlight.7.3.pack.js'
+							'/assets/swagger-ui/dist/lib/shred.bundle.js',
+							'/assets/swagger-ui/dist/lib/jquery.slideto.min.js',
+							'/assets/swagger-ui/dist/lib/jquery.wiggle.min.js',
+							'/assets/swagger-ui/dist/lib/jquery.ba-bbq.min.js',
+							'/assets/swagger-ui/dist/lib/handlebars-1.0.0.js',
+							'/assets/swagger-ui/dist/lib/underscore-min.js',
+							'/assets/swagger-ui/dist/lib/backbone-min.js',
+							'/assets/swagger-ui/dist/lib/swagger.js',
+							'/assets/swagger-ui/dist/swagger-ui.min.js',
+							'/assets/swagger-ui/dist/lib/highlight.7.3.pack.js'
 						];
-						
-						$css = ['components/swagger-ui/dist/css/screen.css'];
-						
+
+						$css = [
+							'/assets/swagger-ui/dist/css/screen.css'
+						];
+		
 						$current = $match['module'];
-						$content = $twig->render('api.twig', [
+						$content = $this->render('/keeko/developer-app/templates/api.twig', [
 							'base' => $this->getAppUrl(),
 							'url' => $this->getAppUrl() . 'reference/' . $match['module'] . '.json',
 							'module' => $this->findModuleBySlug($match['module'])
 						]);
 					} else {
-						$content = $twig->render('reference/index.twig');
+						$content = $this->render('/keeko/developer-app/templates/reference/index.twig');
 					}
-					
-					$main = $twig->render('reference.twig', [
+						
+					$main = $this->render('/keeko/developer-app/templates/reference.twig', [
 						'base' => $this->getAppUrl(),
 						'content' => $content,
 						'modules' => $this->loadModules(),
 						'current' => $current
 					]);
-					
+						
 					break;
-					
+						
 				case 'index':
-					$main = $twig->render('index.twig', [
+					$main = $this->render('/keeko/developer-app/templates/index.twig', [
 						'plattform_name' => $prefs->getPlattformName(),
 						'base' => $this->getAppUrl()
 					]);
 					break;
-					
+						
 				case 'area':
 				case 'topic':
 					$current = isset($match['topic']) ? $match['topic'] : 'index';
-					$content = $twig->render(sprintf('%s/%s.twig', $match['area'], $current), [
+					$content = $this->render(sprintf('/keeko/developer-app/templates/%s/%s.twig', $match['area'], $current), [
 						'base' => $this->getAppUrl(),
 						'api_url' => $prefs->getApiUrl()
 					]);
-					$main = $twig->render(sprintf('%s.twig', $match['area']), [
+					$main = $this->render(sprintf('/keeko/developer-app/templates/%s.twig', $match['area']), [
 						'content' => $content,
 						'menu' => $this->getMenu($match['area']),
 						'current' => $current,
@@ -114,8 +116,8 @@ class DeveloperApplication extends AbstractApplication {
 					]);
 					break;
 			}
-	
-			$response->setContent($twig->render('main.twig', [
+
+			$response->setContent($this->render('/keeko/developer-app/templates/main.twig', [
 				'plattform_name' => $prefs->getPlattformName(),
 				'root' => $this->getRootUrl(),
 				'base' => $this->getAppUrl(),
@@ -125,13 +127,13 @@ class DeveloperApplication extends AbstractApplication {
 				'scripts' => $scripts,
 				'main' => $main
 			]));
-	
+			
 		} catch (ResourceNotFoundException $e) {
 			$response->setStatusCode(Response::HTTP_NOT_FOUND);
 		}
 		return $response;
 	}
-	
+		
 	private function loadModules() {
 		if (!empty($this->modules)) {
 			return $this->modules;
@@ -168,13 +170,12 @@ class DeveloperApplication extends AbstractApplication {
 				return $module;
 			}
 		}
-		
+	
 		return null;
 	}
 	
 	private function generateRoutes() {
 		$routes = new RouteCollection();
-	
 		$routes->add('index', new Route('/'));
 		$routes->add('reference', new Route('/reference'));
 		$routes->add('json', new Route('/reference/{module}.json', [], ['module' => '.+']));
@@ -191,24 +192,23 @@ class DeveloperApplication extends AbstractApplication {
 			case 'guides':
 				$menu = [
 					'auth' => [
-						'title' => 'Authentication',
-						'chapters' => [
-							'basic' => 'Basic',
-							'token' => 'Token'
-						]
-					],
-					'error-handling' => [
-						'title' => 'Error Handling',
-						'chapters' => [
-							'http-status-codes' => 'HTTP-Status Codes',
-							'exceptions' => 'Exceptions'
-						],
+					'title' => 'Authentication',
+					'chapters' => [
+						'basic' => 'Basic',
+						'token' => 'Token'
 					]
-				];
-				break;
+				],
+				'error-handling' => [
+					'title' => 'Error Handling',
+					'chapters' => [
+						'http-status-codes' => 'HTTP-Status Codes',
+						'exceptions' => 'Exceptions'
+					],
+				]
+			];
+			break;
 		}
-
+	
 		return $menu;
 	}
-	
 }
